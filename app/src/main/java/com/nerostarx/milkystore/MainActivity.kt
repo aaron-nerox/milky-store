dc756a34d67bc37062fb823d3c289a47133217f4
Nerostarx <35573892+NeroStarx@users.noreply.github.com>
2021-08-17 19:16:23 +0100

    add: added viewBinding to the project

--- app/src/main/java/com/nerostarx/milkystore/MainActivity.kt
+++ app/src/main/java/com/nerostarx/milkystore/MainActivity.kt
@@ -2,10 +2,15 @@ package com.nerostarx.milkystore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nerostarx.milkystore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
