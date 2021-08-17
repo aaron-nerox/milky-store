127e5c53f3a97163966fb85e5879d2fb2b3a0c12
Nerostarx <35573892+NeroStarx@users.noreply.github.com>
2021-08-17 22:44:31 +0100

    add: Authentication activity

---
+++ app/src/main/java/com/nerostarx/milkystore/activities/AuthActivity.kt
@@ -0,0 +1,16 @@
+package com.nerostarx.milkystore.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nerostarx.milkystore.databinding.ActivityAuthBinding
+
+class AuthActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAuthBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
