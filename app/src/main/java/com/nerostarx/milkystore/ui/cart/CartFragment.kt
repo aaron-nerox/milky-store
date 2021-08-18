db30120ea248af53f364cca1d562385c01ab885c
Nerostarx <35573892+NeroStarx@users.noreply.github.com>
2021-08-18 19:18:49 +0100

    add: binding the adapter with the view

--- app/src/main/java/com/nerostarx/milkystore/ui/cart/CartFragment.kt
+++ app/src/main/java/com/nerostarx/milkystore/ui/cart/CartFragment.kt
@@ -7,7 +7,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager.VERTICAL
 import com.nerostarx.milkystore.R
+import com.nerostarx.milkystore.adapters.CartAdapter
 import com.nerostarx.milkystore.databinding.CartFragmentBinding
 
 class CartFragment : Fragment() {
@@ -23,5 +26,19 @@ class CartFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initRecycler()
+        binding.checkoutButton.setOnClickListener {
+
+        }
+    }
+
+    private fun initRecycler() {
+        val recycler = binding.cartItemsRecycler
+
+        recycler.setHasFixedSize(false)
+        recycler.layoutManager = LinearLayoutManager(requireContext()
+            ,VERTICAL,false)
+        recycler.adapter = CartAdapter()
     }
 }
