6e226594f39054661f0116e440c62e143b7304f7
Nerostarx <35573892+NeroStarx@users.noreply.github.com>
2021-08-18 22:48:48 +0100

    add: login/register destinations

---
+++ app/src/main/java/com/nerostarx/milkystore/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package com.nerostarx.milkystore
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/nerostarx/milkystore/RegisterViewModel.kt
@@ -0,0 +1,7 @@
+package com.nerostarx.milkystore
+
+import androidx.lifecycle.ViewModel
+
+class RegisterViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
