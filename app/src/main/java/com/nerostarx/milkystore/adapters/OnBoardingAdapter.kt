82353590bd3be16ccf5c84e121f90d90bd5d96c8
Nerostarx <35573892+NeroStarx@users.noreply.github.com>
2021-08-18 14:15:00 +0100

    add: onBoarding adapter

---
+++ app/src/main/java/com/nerostarx/milkystore/adapters/OnBoardingAdapter.kt
@@ -0,0 +1,41 @@
+package com.nerostarx.milkystore.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.nerostarx.milkystore.databinding.OnboardingItemBinding
+
+class OnBoardingAdapter(): RecyclerView.Adapter<OnBoardingAdapter.OnBoardingViewHolder>() {
+
+    //val animationList = ArrayList<Int>()
+    val titleList = arrayListOf("Welcome to milky store"
+        , "Top quality milk flavors"
+        , "Discounts, a lot of them!")
+
+    val textList = arrayListOf(
+        "Milky store welcomes you to a world full of milk, orange and some of the sweetest things."
+        , "Get to enjoy some of the best milk flavors that you will taste in you life, and the best part is that they are home made."
+        , "Here at milky store we know that our clients will go all out for us, So we do the same for them, by giving them discounts."
+    )
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OnBoardingViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = OnboardingItemBinding.inflate(layoutInflater,parent,false)
+        return OnBoardingViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: OnBoardingViewHolder, position: Int) {
+        holder.title.text = titleList[position]
+        holder.text.text = textList[position]
+    }
+
+    override fun getItemCount(): Int = 3
+
+    class OnBoardingViewHolder(binding: OnboardingItemBinding)
+        :RecyclerView.ViewHolder(binding.root) {
+
+            val animation = binding.animationView
+            val title = binding.titleOnboarding
+            val text = binding.textOnboarding
+    }
+}
